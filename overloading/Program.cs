e541c9398a539233a4eb770702fde193805bb3dc
rayli <rayli@LAPTOP-K395GLJO>
2023-03-07 19:45:59 +0800

    Add project files.

---
+++ overloading/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+class OverloadDemo
+{
+    public void Print(int value)
+    {
+        Console.WriteLine($"Printing integer value: {value}");
+    }
+
+    public void Print(double value)
+    {
+        Console.WriteLine($"Printing double value: {value}");
+    }
+
+    public void Print(string value)
+    {
+        Console.WriteLine($"Printing string value: {value}");
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        OverloadDemo demo = new OverloadDemo();
+
+        demo.Print(5);           // calls the integer Print method
+        demo.Print(3.14);        // calls the double Print method
+        demo.Print("hello");     // calls the string Print method
+    }
+}
